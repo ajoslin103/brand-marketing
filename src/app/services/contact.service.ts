c6ac2eb92d99f12bbd7fc5aa033b4fea62fe1aaa
Al Joslin <allen.joslin@gmail.com>
2020-05-30 14:55:55 -0400

    configure service for our needs

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { ContactRecord } from '../models/contact';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
-  constructor() {}
+  private serviceUrl = 'http://demo5838836.mockable.io/contact';
+
+  constructor(private http: HttpClient) {}
+
+  getContactsTableItem(): Observable<ContactRecord[]> {
+    return this.http.get<ContactRecord[]>(this.serviceUrl);
+  }
 }
