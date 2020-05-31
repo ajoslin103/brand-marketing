7acb5c4cd68a682749b6bf7383b794827b29e619
Al Joslin <allen.joslin@gmail.com>
2020-05-31 09:03:06 -0400

    clean service, return service url

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -4,15 +4,20 @@ import { Observable } from 'rxjs';
 
 import { ContactRecord } from '../models/contact';
 
+import { tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
-  private serviceUrl = 'http://demo5838836.mockable.io/contact';
+  // private serviceUrl = 'http://demo5838836.mockable.io/contact';
+  private serviceUrl = 'http://localhost:3000/contacts';
 
   constructor(private http: HttpClient) {}
 
-  getContactsTableItem(): Observable<ContactRecord[]> {
+  getServiceUrl = () => this.serviceUrl;
+
+  getContactRecordObservable(): Observable<ContactRecord[]> {
     return this.http.get<ContactRecord[]>(this.serviceUrl);
   }
 }
