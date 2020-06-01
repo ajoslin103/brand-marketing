7e6e042444e102f25fcc0fd446a8399244431ec7
Al Joslin <allen.joslin@gmail.com>
2020-06-01 17:28:26 -0400

    condense all connection to dataSource in one call
    all else revert to boilerplate

--- src/app/datasources/contact-datasource.spec.ts
+++ src/app/datasources/contact-datasource.spec.ts
@@ -1,89 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { Injector } from '@angular/core';
 
 import { ContactDataSource } from './contact-datasource';
-import { ContactRecord } from '../models/contact';
-
-const SAMPLE_DATA = [
-  {
-    _id: '5de91c005b98615393e74931',
-    index: 0,
-    firstName: 'Browning',
-    lastName: 'Graham',
-    company: 'MELBACOR',
-    email: 'browninggraham@melbacor.com',
-    phone: '+1 (906) 585-2525',
-    address: '920 Hastings Street, Roosevelt, Puerto Rico, 5573',
-  },
-  {
-    _id: '5de91c00d6b4d04e96ef44da',
-    index: 1,
-    firstName: 'Mcmahon',
-    lastName: 'Fulton',
-    company: 'ILLUMITY',
-    email: 'mcmahonfulton@illumity.com',
-    phone: '+1 (814) 489-3373',
-    address: '676 Bainbridge Street, Abrams, Mississippi, 2652',
-  },
-];
+import { ContactService } from '../services/contact.service';
 
 describe('ContactDataSource', () => {
-  let datasource = new ContactDataSource(SAMPLE_DATA);
-  // datasource.paginator = new MatPaginator(
-  //   MatPaginatorIntl,
-  //   ChangeDetectorRef,
-  //   MatPaginatorDefaultOptions
-  // );
-  // datasource.sort = new MatSort();
+  let injector: Injector;
+  let dataSource: ContactDataSource;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    injector = TestBed.configureTestingModule({
+      providers: [ContactService],
       imports: [HttpClientModule],
     });
-    datasource = new ContactDataSource([]);
-  });
+    dataSource = injector.get(ContactDataSource);
+  }));
 
   it('should be created', () => {
-    expect(datasource).toBeTruthy();
+    expect(dataSource).toBeTruthy();
   });
 
-  it('should have a [was private] method: getPagedData', () => {
-    expect(typeof datasource.getPagedData === 'function').toBeTrue();
-  });
-
-  it('should have a [was private] method: getSortedData', () => {
-    expect(typeof datasource.getSortedData === 'function').toBeTrue();
-  });
-
-  it('should have a method: connect', () => {
-    expect(typeof datasource.connect === 'function').toBeTrue();
-  });
-
-  it('should have a method: disconnect', () => {
-    expect(typeof datasource.disconnect === 'function').toBeTrue();
-  });
-
-  it('should have a function: compare', () => {
-    expect(typeof datasource.compare === 'function').toBeTrue();
-  });
-
-  it('should compare a to b', () => {
-    expect(datasource.compare('a', 'b', true)).toBe(-1);
-  });
-
-  it('should compare 1 to 2', () => {
-    expect(datasource.compare(1, 2, true)).toBe(-1);
-  });
-
-  // it('should getPagedData', () => {
-  //   datasource.paginator.pageSize = 10;
-  //   datasource.paginator.pageIndex = 0;
-  //   expect(datasource.getPagedData(SAMPLE_DATA)).toBeUndefined();
-  // });
-
-  // it('should getSortedData', () => {
-  //   expect(datasource.getSortedData(SAMPLE_DATA)).toBeUndefined();
-  // });
+  // TODO: using the datasource will be tested in the ContactsTableComponent
+  // as it needs connection to a MatTable, MatPaginator, and MatSort
+  // before it loads the records into it's `data` field
 });
--- src/app/datasources/contact-datasource.ts
+++ src/app/datasources/contact-datasource.ts
@@ -1,10 +1,13 @@
+import { Injectable } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
 
 import { ContactRecord } from '../models/contact';
+import { ContactService } from '../services/contact.service';
 
 /**
  * Data source for the ContactsTable view. This class should
@@ -12,12 +15,41 @@ import { ContactRecord } from '../models/contact';
  * (including sorting, pagination, and filtering).
  */
 
+@Injectable({
+  providedIn: 'root',
+})
 export class ContactDataSource extends DataSource<ContactRecord> {
+  table: MatTable<ContactRecord>;
+  data: ContactRecord[] = [];
   paginator: MatPaginator;
   sort: MatSort;
 
-  constructor(public data: ContactRecord[]) {
+  constructor(private contactService: ContactService) {
     super();
+  }
+
+  /**
+   * This routine requestst the data, hooks up the paginator, sorter, and table
+   * Then replaces the tables dataSource when records have been loaded
+   * There are Waaaaay better ways to do this when I get better at moden Angular
+   *
+   * @param paginator passed down from the owning table componant
+   * @param sort passed down from the owning table componant
+   * @param table passed down from the owning table componant
+   */
+  init({ paginator, sort, table }): Promise<ContactRecord[]> {
+    return new Promise((resolve, reject) => {
+      this.contactService.getContactRecordObservable().subscribe((records) => {
+        this.data.push(...records);
+        this.table.dataSource = this;
+        resolve(records);
+      });
+
+      this.paginator = paginator;
+      this.table = table;
+      this.sort = sort;
+      this.connect();
+    });
   }
 
   /**
@@ -51,7 +83,7 @@ export class ContactDataSource extends DataSource<ContactRecord> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  public getPagedData(data: ContactRecord[]) {
+  private getPagedData(data: ContactRecord[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
   }
@@ -60,7 +92,7 @@ export class ContactDataSource extends DataSource<ContactRecord> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  public getSortedData(data: ContactRecord[]) {
+  private getSortedData(data: ContactRecord[]) {
     if (!this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -82,21 +114,17 @@ export class ContactDataSource extends DataSource<ContactRecord> {
       const isAsc = this.sort.direction === 'asc';
       switch (true) {
         case 'string' === typeof a[this.sort.active]:
-          return this.compare(a[this.sort.active], b[this.sort.active], isAsc);
+          return compare(a[this.sort.active], b[this.sort.active], isAsc);
         case 'number' === typeof a[this.sort.active]:
-          return this.compare(
-            a[+this.sort.active],
-            b[+this.sort.active],
-            isAsc
-          );
+          return compare(a[+this.sort.active], b[+this.sort.active], isAsc);
         default:
           return 0;
       }
     });
   }
+}
 
-  /** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-  public compare(a: string | number, b: string | number, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
+/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+function compare(a: string | number, b: string | number, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
