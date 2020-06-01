67a81a50f07bcd93380c02d94922d0ac33215a9d
Al Joslin <allen.joslin@gmail.com>
2020-06-01 19:22:19 -0400

    consume UIStrings in main window

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { UIString, UIStrings, CONTACTS_PAGE_TITLE } from './utils/ui-strings';
+import { Injector } from '@angular/core';
 
 describe('AppComponent', () => {
+  let injector: Injector;
+  let enStrings: UIStrings;
+  let contactsPageTitle: UIString;
+
   beforeEach(async(() => {
+    injector = TestBed.configureTestingModule({
+      providers: [UIStrings],
+    });
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
+
+    enStrings = injector.get(UIStrings);
+    contactsPageTitle = enStrings.Get(CONTACTS_PAGE_TITLE);
   }));
 
   it('should create the app', () => {
@@ -16,9 +29,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Brand Marketing [ajoslin]'`, () => {
+  // there are arguments against complex testing...
+  // helpers should be built to reduce/remove this technical debt
+  // TODO: do this if I have time
+
+  it(`should have as title '${contactsPageTitle}'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Brand Marketing [ajoslin]');
+    expect(app.title).toEqual(contactsPageTitle);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { UIString, UIStrings, CONTACTS_PAGE_TITLE } from './utils/ui-strings';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Brand Marketing [ajoslin]';
+  title: UIString;
+
+  constructor(private enStrings: UIStrings) {
+    this.title = enStrings.Get(CONTACTS_PAGE_TITLE);
+  }
 }
