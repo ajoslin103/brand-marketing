bff17a9285fb57e35983c0f6aae20d0f62b6c5b8
Al Joslin <allen.joslin@gmail.com>
2020-06-03 12:43:10 -0400

    emit & consume newContacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { UIString, UIStrings, CONTACTS_PAGE_TITLE } from './utils/ui-strings';
+import { ContactAddedService } from './services/contact-added.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
   title: UIString;
 
-  constructor(private enStrings: UIStrings) {
+  constructor(private enStrings: UIStrings, private contactAddedService: ContactAddedService) {
     this.title = enStrings.Get(CONTACTS_PAGE_TITLE);
+  }
+
+  ngOnInit() {
+    this.subscription = this.contactAddedService.newContact.subscribe(
+      (newContact) => {
+        console.log(`Contact Submitted: ${JSON.stringify(newContact)}`);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
